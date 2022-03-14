d42f6b050b5cb0b67e25f9497d4dd6e968e8295b
Kate <katensullivan55@gmail.com>
2022-03-14 14:46:11 -0400

    quiz js files added and filled in

---
+++ assets/js/questions.js
@@ -0,0 +1,51 @@
+const questions = [
+    {
+        title: 'What is the process of finding errors in code called?',
+        choices: ['sorting', 'fixing', 'debugging', 'scanning'],
+        answer: 'debugging'
+    },
+    {
+        title: 'What symbol do you use in css to reference an id in the linked html file?',
+        choices: ['#', '.', '$', ':'],
+        answer: '#'
+    },
+    {
+        title: 'What is the best practice name for a html file?',
+        choices: ['html.html', 'index.html', 'basecode.html', 'website.html'],
+        answer: 'index.html'
+    },
+    {
+        title: 'How many css documents can you link to one html document?',
+        choices: ['1', '5', '20', 'As many as you need'],
+        answer: 'As many as you need'
+    },
+    {
+        title: 'What is the most current ',
+        choices: ['#id', '$id', '.id', '?id'],
+        answer: '#id'
+    },
+    {
+        title: 'What is the correct way to use a class in HTML/CSS?',
+        choices: ['#class', '$class', '.class', '?class'],
+        answer: '.class'
+    },
+    {
+        title: 'Which of these would NOT set a color in HTML/CSS?',
+        choices: ['#000000', 'black', 'rgb(255, 99, 71)', '@2'],
+        answer: '@2'
+    },
+    {
+        title: 'What does HTML stand for?',
+        choices: ['Hypertext Markup Language', 'Hydrotext Manipulation Language', 'Home Text Manuscript Learning', 'Holy Trout, My Lentils!'],
+        answer: 'Hypertext Markup Language'
+    }, {
+        title: 'Which tag has the largest heading?',
+        choices: ['h1', 'h2', 'mega-h', 'hINFINITY'],
+        answer: 'h1'
+    },
+    {
+        title: 'Which HTML element do we store JavaScript in?',
+        choices: ['<script>', '<js>', '<javascript>', '<scripts>'],
+        answer: '<script>'
+    },
+];
